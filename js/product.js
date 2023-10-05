7546f37b6cdd767847cc8420355d038972e8d857
Vendifo <maloy5916@inbox.ru>
2023-10-06 02:03:50 +0300

    J,obt yfxthnfybz (Djn b hfp,bhfqcz xnj z nen yfgbcfk? ghjuhfvvbcn [etd])

--- js/product.js
+++
@@ -1,8 +0,0 @@
-var i=0;
-        var image=document.getElementById("heartImage");
-        // Добавте свои картинки через запятую
-        var imgs=new Array('/img/heart1.svg','/img/heart1full.png');
-        function changeImage() {
-            i++;
-            image.src=imgs[i];
-        }
