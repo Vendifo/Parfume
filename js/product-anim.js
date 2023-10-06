f7851135cbd572ab9e9fb2384c56621a5897a4ac
Vendifo <maloy5916@inbox.ru>
2023-10-06 06:24:26 +0300

    Слыш чмо, если ты это читаешь, то умничка, по фунн кционалу странички карточки товара: Сама карта епт Реализация базовых функций, типа ссылки, шапки ну это при тебя Далее самое интересное: Форма самой карточки слизанная из фигмы Добавлен мастаб при наведении курсора на картинку, Добавлена анимация наведения нна кнопочки, При нажатии добавить в корзину или в понравившееся, выходит соответствующее уведомление Добавлены мини слайды других изо и переключение между ними Добавил линий, прости Реалтзовали другое, там кнопки тоже настроил и добавил анимация ФСё

---
+++ js/product-anim.js
@@ -0,0 +1,74 @@
+/*Анимация сообщения о добавлении в wish and cart*/
+
+function addToCart() {
+    var cartMessage = document.getElementById("cartMessage");
+    
+    // Показать сообщение
+    cartMessage.style.display = "block";
+
+    // Задать непрозрачность на 1 (плавное появление)
+    cartMessage.style.opacity = "1";
+
+    // Скрыть сообщение через несколько секунд (например, 3 секунды)
+    setTimeout(function() {
+        // Установить непрозрачность на 0 (плавное исчезновение)
+        cartMessage.style.opacity = "0";
+        
+        // Скрыть сообщение после завершения анимации
+        setTimeout(function() {
+            cartMessage.style.display = "none";
+        }, 500); // Время анимации в миллисекундах (0.5 секунды)
+    }, 3000); // Время отображения сообщения в миллисекундах (3 секунды)
+}
+
+function addtoWish() {
+    var cartMessage = document.getElementById("wishMessage");
+    
+    // Показать сообщение
+    cartMessage.style.display = "block";
+
+    // Задать непрозрачность на 1 (плавное появление)
+    cartMessage.style.opacity = "1";
+
+    // Скрыть сообщение через несколько секунд (например, 3 секунды)
+    setTimeout(function() {
+        // Установить непрозрачность на 0 (плавное исчезновение)
+        cartMessage.style.opacity = "0";
+        
+        // Скрыть сообщение после завершения анимации
+        setTimeout(function() {
+            cartMessage.style.display = "none";
+        }, 500); // Время анимации в миллисекундах (0.5 секунды)
+    }, 3000); // Время отображения сообщения в миллисекундах (3 секунды)
+}
+
+/*Увеличение размера изображения*/
+const imageContainer = document.getElementById("imageContainer");
+const image = document.getElementById("image");
+
+imageContainer.addEventListener("mousemove", (e) => {
+    const { left, top, width, height } = imageContainer.getBoundingClientRect();
+    const x = (e.clientX - left) / width;
+    const y = (e.clientY - top) / height;
+
+    // Уменьшаем масштаб и позицию изображения в зависимости от положения курсора
+    image.style.transform = `scale(1.2) translate(-${x * 25}%, -${y * 25}%)`;
+});
+/*Возврат масштаба при отсутствии курсора*/
+imageContainer.addEventListener("mouseleave", () => {
+    // Сбрасываем масштаб и позицию при выходе курсора из контейнера
+    image.style.transform = "scale(1) translate(0%, 0%)";
+});
+
+function changeImage(newImageSrc) {
+    
+    
+    // Установите новый источник изображения
+    image.src = newImageSrc;
+
+    // Плавно показать изображение с эффектом
+    imageContainer.style.opacity = 0; // Сначала делаем элемент невидимым
+    setTimeout(function () {
+        imageContainer.style.opacity = 1; // Затем делаем элемент видимым снова
+    }, 300); // 300 миллисекунд - это продолжительность анимации
+}
