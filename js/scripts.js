5ee71a58c3a74c2154c011b8242ec98b478a3a89
ParadoksI <wwesuper146@gmail.com>
2023-11-01 22:45:03 +0900

    Денис кхм-кзм а еще я начал делать слайдер

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,13 @@ function hideTexts() {
   text2.classList.remove("fade-in");
 }
 
+text1.style.display = "block";
+  setTimeout(() => {
+    text1.style.opacity = "1";
+    text1.classList.add("fade-in");
+  }, 10);
+
+
 // Добавляем обработчики событий для кнопок
 bestsellers_button.addEventListener("click", function() {
   // Скрываем текущий текст
@@ -47,3 +54,66 @@ function changeColor(button) {
     button.style.color = 'black';
   }
 }
+
+const slider = document.querySelector('.slider1');
+const slides = document.querySelectorAll('.slide1');
+let isDragging = false;
+let startPosition = 0;
+let currentTranslate = 0;
+let prevTranslate = 0;
+let animationID = 0;
+let currentIndex = 0;
+
+slides.forEach((slide, index) => {
+  slide.addEventListener('touchstart', touchStart(index));
+  slide.addEventListener('touchend', touchEnd);
+  slide.addEventListener('touchmove', touchMove);
+});
+
+window.addEventListener('resize', setPositionByIndex);
+
+function touchStart(index) {
+  return function (event) {
+    currentIndex = index;
+    startPosition = event.touches[0].clientX;
+    isDragging = true;
+    animationID = requestAnimationFrame(updateSlider);
+  };
+}
+
+function touchEnd() {
+  isDragging = false;
+  cancelAnimationFrame(animationID);
+
+  const movedBy = currentTranslate - prevTranslate;
+  if (movedBy < -100 && currentIndex < slides.length - 1) {
+    currentIndex += 1;
+  } else if (movedBy > 100 && currentIndex > 0) {
+    currentIndex -= 1;
+  }
+
+  setPositionByIndex();
+}
+
+function touchMove(event) {
+  if (isDragging) {
+    const currentPosition = event.touches[0].clientX;
+    currentTranslate = prevTranslate + currentPosition - startPosition;
+  }
+}
+
+function updateSlider() {
+  const translateX = currentTranslate * -1;
+  slider.style.transform = `translateX(${translateX}px)`;
+  animationID = requestAnimationFrame(updateSlider);
+}
+
+function setPositionByIndex() {
+  currentTranslate = currentIndex * -100;
+  prevTranslate = currentTranslate;
+  updateSlider();
+}
+
+setPositionByIndex();
+
+
