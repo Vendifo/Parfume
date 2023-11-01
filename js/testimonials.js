1380f73be5db6a87c71b88d5558f0e8d005d8e04
ParadoksI <wwesuper146@gmail.com>
2023-11-02 00:36:35 +0900

    Сохранение скрипта перед тестами

--- js/testimonials.js
+++ js/testimonials.js
@@ -11,65 +11,74 @@ let velocity = 0;
 const containerWidth = testimonialsContainer.clientWidth;
 const listWidth = testimonialsList.scrollWidth;
 
-testimonialsContainer.addEventListener('mousedown', (e) => {
-    isDragging = true;
-    startPosition = e.clientX - testimonialsContainer.getBoundingClientRect().left; // Учтите позицию блока
-    previousTranslateX = currentTranslateX;
-    velocity = 0;
-    testimonialsList.style.transition = 'none';
-});
+const startDrag = (event) => {
+  isDragging = true;
+  startPosition = event.clientX || event.touches[0].clientX;
+  previousTranslateX = currentTranslateX;
+  velocity = 0;
+  testimonialsList.style.transition = 'none';
+};
 
-document.addEventListener('mousemove', (e) => {
-    if (!isDragging) return;
+const drag = (event) => {
+  if (!isDragging) return;
 
-    const currentPosition = e.clientX - testimonialsContainer.getBoundingClientRect().left; // Учтите позицию блока
-    currentTranslateX = previousTranslateX + currentPosition - startPosition;
+  const currentPosition = event.clientX || event.touches[0].clientX;
+  currentTranslateX = previousTranslateX + currentPosition - startPosition;
+
+  // Проверка, можно ли скроллить вправо или влево
+  if (currentTranslateX > 0) {
+    currentTranslateX = 0; // Нельзя скроллить влево за границу
+  } else if (currentTranslateX < containerWidth - listWidth) {
+    currentTranslateX = containerWidth - listWidth; // Нельзя скроллить вправо за границу
+  }
+
+  testimonialsList.style.transform = `translateX(${currentTranslateX}px)`;
+};
+
+const endDrag = () => {
+  if (!isDragging) return;
+  isDragging = false;
+
+  // Расчет скорости для инерции
+  const currentTime = new Date().getTime();
+  const timeDiff = currentTime - previousTimestamp;
+  const distance = currentTranslateX - previousTranslateX;
+  velocity = distance / timeDiff;
+
+  // Ваша логика здесь (например, проверка, сколько элементов должно быть видно)
+
+  testimonialsList.style.transition = 'transform 0.3s ease-in-out';
+  testimonialsList.style.transform = `translateX(${currentTranslateX}px)`;
+};
+
+testimonialsContainer.addEventListener('mousedown', startDrag);
+document.addEventListener('mousemove', drag);
+document.addEventListener('mouseup', endDrag);
+
+testimonialsContainer.addEventListener('touchstart', startDrag);
+document.addEventListener('touchmove', drag);
+document.addEventListener('touchend', endDrag);
+
+let previousTimestamp = 0;
+
+function animateScroll() {
+  if (!isDragging) {
+    // Применение инерции с уменьшенной скоростью
+    currentTranslateX += velocity * 0.01; // Уменьшите коэффициент для меньшей скорости инерции
 
     // Проверка, можно ли скроллить вправо или влево
     if (currentTranslateX > 0) {
-        currentTranslateX = 0; // Нельзя скроллить влево за границу
+      currentTranslateX = 0; // Нельзя скроллить влево за границу
     } else if (currentTranslateX < containerWidth - listWidth) {
-        currentTranslateX = containerWidth - listWidth; // Нельзя скроллить вправо за границу
+      currentTranslateX = containerWidth - listWidth; // Нельзя скроллить вправо за границу
     }
-
-    testimonialsList.style.transform = `translateX(${currentTranslateX}px)`;
-});
-
-document.addEventListener('mouseup', () => {
-    if (!isDragging) return;
-    isDragging = false;
-
-    // Расчет скорости для инерции
-    const currentTime = new Date().getTime();
-    const timeDiff = currentTime - previousTimestamp;
-    const distance = currentTranslateX - previousTranslateX;
-    velocity = distance / timeDiff;
-
-    // Ваша логика здесь (например, проверка, сколько элементов должно быть видно)
 
     testimonialsList.style.transition = 'transform 0.3s ease-in-out';
     testimonialsList.style.transform = `translateX(${currentTranslateX}px)`;
-});
+  }
 
-let previousTimestamp = 0;
-function animateScroll() {
-    if (!isDragging) {
-        // Применение инерции с уменьшенной скоростью
-        currentTranslateX += velocity * 0.01; // Уменьшите коэффициент для меньшей скорости инерции
-
-        // Проверка, можно ли скроллить вправо или влево
-        if (currentTranslateX > 0) {
-            currentTranslateX = 0; // Нельзя скроллить влево за границу
-        } else if (currentTranslateX < containerWidth - listWidth) {
-            currentTranslateX = containerWidth - listWidth; // Нельзя скроллить вправо за границу
-        }
-
-        testimonialsList.style.transition = 'transform 0.3s ease-in-out';
-        testimonialsList.style.transform = `translateX(${currentTranslateX}px)`;
-    }
-
-    previousTimestamp = new Date().getTime();
-    requestAnimationFrame(animateScroll);
+  previousTimestamp = new Date().getTime();
+  requestAnimationFrame(animateScroll);
 }
 
 animateScroll();
