8ea580ed00696540204111b9ea319331e15b66f0
Vendifo <maloy5916@inbox.ru>
2023-11-26 05:51:33 +0900

    Страница наборов

---
+++ js/sets.js
@@ -0,0 +1,8 @@
+function toggleText() {
+    var hiddenText = document.getElementById("hiddenText");
+    if (hiddenText.style.display === "none") {
+        hiddenText.style.display = "block";
+    } else {
+        hiddenText.style.display = "none";
+    }
+}
