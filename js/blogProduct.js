1afa37e4be68cc297b0c49e3704b70e1eb98f33f
ParadoksI <wwesuper146@gmail.com>
2023-11-27 07:07:20 +0900

    Добавлены анимации а так же Взят обсервер на постоянную бесплатную подработку

--- js/blogProduct.js
+++ js/blogProduct.js
@@ -1,7 +1,78 @@
-function showButton() {
-    document.getElementById('buttonContainer').style.display = 'flex';
+
+
+function toggleText() {
+    var fullText = document.querySelector('.blog-item__disc__text.full-text');
+    var textButton = document.querySelector('.blog-item__disc__button');
+
+    fullText.style.maxHeight = fullText.scrollHeight + 'px';
+    textButton.style.display = "none";
+
 }
 
-function hideButton() {
-    document.getElementById('buttonContainer').style.display = 'none';
-}
+document.addEventListener('DOMContentLoaded', function () {
+    // Находим все элементы, которые должны анимироваться
+    var animatedElements = document.querySelectorAll('.blog-item__product__reviews');
+
+    // Создаем экземпляр Intersection Observer
+    var observer = new IntersectionObserver(function (entries, observer) {
+        // Перебираем все записи (элементы), которые находятся в зоне видимости
+        entries.forEach(function (entry) {
+            // Проверяем, стал ли элемент видимым
+            if (entry.isIntersecting) {
+                // Добавляем класс с анимацией
+                entry.target.style.opacity = '1';
+                entry.target.style.transform = 'rotate(0deg) ';
+                // После того как элемент виден, не нужно больше следить за ним
+                observer.unobserve(entry.target);
+            }
+        });
+    }, { threshold: 0.5 }); // Порог видимости 50%
+
+    // Начинаем наблюдение за каждым элементом
+    animatedElements.forEach(function (element) {
+        observer.observe(element);
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Находим все элементы, которые должны анимироваться
+    var animatedElements = document.querySelectorAll('.blog-item__product__reviews2');
+
+    // Создаем экземпляр Intersection Observer
+    var observer = new IntersectionObserver(function (entries, observer) {
+        // Перебираем все записи (элементы), которые находятся в зоне видимости
+        entries.forEach(function (entry) {
+            // Проверяем, стал ли элемент видимым
+            if (entry.isIntersecting) {
+                // Добавляем класс с анимацией
+                entry.target.style.opacity = '1';
+                entry.target.style.transform = 'rotate(0deg) ';
+                // После того как элемент виден, не нужно больше следить за ним
+                observer.unobserve(entry.target);
+            }
+        });
+    }, { threshold: 0.5 }); // Порог видимости 50%
+
+    // Начинаем наблюдение за каждым элементом
+    animatedElements.forEach(function (element) {
+        observer.observe(element);
+    });
+});
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var reviewsContainer = document.getElementById('reviewsContainer1');
+    
+    // Задержка перед началом анимации (время в миллисекундах)
+    var delay = 250;
+
+    setTimeout(function () {
+        reviewsContainer.style.opacity = '1';
+        reviewsContainer.style.transform = 'rotate(0deg) scaleX(-1)';
+        
+    }, delay);
+});
--- js/sets.js
+++ js/sets.js
@@ -1,8 +1,56 @@
 function toggleText() {
     var hiddenText = document.getElementById("hiddenText");
-    if (hiddenText.style.display === "none") {
-        hiddenText.style.display = "block";
-    } else {
-        hiddenText.style.display = "none";
-    }
+    hiddenText.classList.toggle("visible1");
 }
+const setInfoMain = document.querySelector('.set-info__main');
+const setInfoVariants = document.querySelector('.set-info__variants');
+
+
+// Добавить классы для запуска анимаций
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Находим все элементы, которые должны анимироваться
+    var animatedElements = document.querySelectorAll('.set-info__main');
+    var animatedElements1 = document.querySelectorAll('.set-info__variants');
+
+    // Создаем экземпляр Intersection Observer
+    var observer = new IntersectionObserver(function (entries, observer) {
+        // Перебираем все записи (элементы), которые находятся в зоне видимости
+        entries.forEach(function (entry) {
+            // Проверяем, стал ли элемент видимым
+            if (entry.isIntersecting) {
+                // Добавляем класс с анимацией
+                entry.target.classList.add('expanded');
+                // После того как элемент виден, не нужно больше следить за ним
+                observer.unobserve(entry.target);
+            }
+        });
+    }, { threshold: 0.5 }); // Порог видимости 50%
+
+    var observer1 = new IntersectionObserver(function (entries, observer) {
+        // Перебираем все записи (элементы), которые находятся в зоне видимости
+        entries.forEach(function (entry) {
+            // Проверяем, стал ли элемент видимым
+            if (entry.isIntersecting) {
+                // Добавляем класс с анимацией
+                entry.target.classList.add('visible');
+                // После того как элемент виден, не нужно больше следить за ним
+                observer.unobserve(entry.target);
+            }
+        });
+    }, { threshold: 0.5 }); // Порог видимости 50%
+    // Начинаем наблюдение за каждым элементом
+    animatedElements.forEach(function (element) {
+        observer.observe(element);
+    });
+    animatedElements1.forEach(function (element) {
+        observer1.observe(element);
+    });
+});
+
+var setTotal = document.querySelector('.set-total');
+
+// Добавьте класс для анимации
+setTotal.classList.add('visible2');
+
